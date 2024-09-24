4d92936b1a432dffb4c95d8cbf257b5f64363c18
yujinwoo <118411971+jin182@users.noreply.github.com>
2024-09-24 23:01:56 +0900

    Update sleepingshark.py

--- sleepingshark.py
+++ sleepingshark.py
@@ -2,12 +2,11 @@ from pwn import *
 import dpkt
 import datetime
 import urllib.parse
+from collections import defaultdict
 
 def analyze_packets(pcap):
     flag = ['X'] * 39
-    post_sent = False
-    payload = ''
-    request_time = 0
+    request_data = defaultdict(dict)
     
     for timestamp, buf in pcap:
         eth = dpkt.ethernet.Ethernet(buf)
@@ -18,38 +17,35 @@ def analyze_packets(pcap):
             continue
         tcp = ip.data
         
-        if not post_sent and tcp.dport == 80 and len(tcp.data) > 0:
-            time = datetime.datetime.utcfromtimestamp(timestamp)
-            print(f'Timestamp: {time} ({timestamp})')
-            request_time = timestamp
-            
+        if tcp.dport == 80 and len(tcp.data) > 0:
             try:
                 http = dpkt.http.Request(tcp.data)
                 payload = urllib.parse.unquote(http.uri)
-                print(f'-- request --\n {http.method} {payload}\n')
                 if http.method == 'POST':
-                    post_sent = True
+                    request_data[tcp.sport]['time'] = timestamp
+                    request_data[tcp.sport]['payload'] = payload
             except dpkt.dpkt.UnpackError:
                 continue
                 
-        elif post_sent and tcp.sport == 80 and len(tcp.data) > 0:
-            response_time = timestamp
+        elif tcp.sport == 80 and len(tcp.data) > 0:
+            if tcp.dport not in request_data:
+                continue
             
             try:
                 http = dpkt.http.Response(tcp.data)
-                print(f'-- response --\n{http.status}')
+                request_time = request_data[tcp.dport]['time']
                 
-                if response_time - request_time >= 2.8:
+                if timestamp - request_time >= 2.8:
+                    payload = request_data[tcp.dport]['payload']
                     payload = payload[payload.find('LIMIT 1),') + 9:]
                     idx = int(payload[:payload.find(',')]) - 1
                     ch = chr(int(payload[payload.find('))=') + 3:payload.find(', SLEEP(3)')]))
                     flag[idx] = ch
-                    print(f'\n\nFound!!\n\n flag[{idx}] : {ch}\n\ncurrent flag : {"".join(flag)}')
-                    sleep(0.1)
+                    print(f'\nFound!! flag[{idx}] : {ch}\nCurrent flag: {"".join(flag)}')
+                
+                del request_data[tcp.dport]
             except dpkt.dpkt.UnpackError:
                 continue
-            
-            post_sent = False
     
     return ''.join(flag)
 
@@ -57,7 +53,7 @@ def main():
     pcap_file = '/Users/USER/Downloads/dump.pcap'
     with open(pcap_file, 'rb') as f:
         pcap = dpkt.pcap.Reader(f)
-        print('flag : ' + analyze_packets(pcap))
+        print('Flag:', analyze_packets(pcap))
 
 if __name__ == '__main__':
-    main()​
+    main()
